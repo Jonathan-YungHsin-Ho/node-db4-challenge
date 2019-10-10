472ab3001a7ec748a620d1e0646f2b216417d7ae
Jonathan Ho <johnnyho82@gmail.com>
2019-10-10 18:26:41 -0400

    Add GET /api/ingredients/:id/recipes to achieve MVP and all Stretch Problems

--- ingredients/ingredient-model.js
+++ ingredients/ingredient-model.js
@@ -4,4 +4,10 @@ module.exports = {
   getRecipesByIngredient,
 };
 
-function getRecipesByIngredient(ingredient_id) {}
+function getRecipesByIngredient(ingredient_id) {
+  return db('recipes')
+    .join('recipe_ingredients as ri', 'ri.recipe_id', 'recipes.id')
+    .join('ingredients as i', 'i.id', 'ri.ingredient_id')
+    .select('i.name as ingredient', 'recipes.name as recipe')
+    .where({ ingredient_id });
+}
--- ingredients/ingredient-router.js
+++ ingredients/ingredient-router.js
@@ -5,5 +5,21 @@ const Ingredients = require('./ingredient-model');
 const router = express.Router();
 
 // GET /api/ingredients/:id/recipes endpoint -
+router.get('/:id/recipes', (req, res) => {
+  Ingredients.getRecipesByIngredient(req.params.id)
+    .then(recipes => {
+      if (recipes.length) {
+        res.status(200).json(recipes);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'Could not find recipes for given ingredient' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Failed to get recipes by ingredient' });
+    });
+});
 
 module.exports = router;
